01431da179da926c1ce2aa80645ef997d6086373
Mike Vaughan <dvaugha1@gmail.com>
2015-02-17 16:17:42 -0500

    Added user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :models
   root to: "users#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
