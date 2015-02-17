1d1535d94f8e40e9c0017d4edcf3a9c62589b20e
samuel <samuel.whitley89@gmail.com>
2015-02-17 18:09:02 -0500

    resolved model issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :models
+
+
+
   root to: "users#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
