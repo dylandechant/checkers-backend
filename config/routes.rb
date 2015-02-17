1f7477c69a5df2fd9f75159be616db48e0d01676
Dylan Dechant <dylan.dechant@gmail.com>
2015-02-17 17:19:08 -0500

    add user models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :models
   root to: "users#index"
   # The priority is based upon order of creation: first created -> highest priority.
