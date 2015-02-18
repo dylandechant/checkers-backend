fd384ae027e725faf32f9c9cc6cb66e1ee564fbe
Dylan Dechant <dylan.dechant@gmail.com>
2015-02-18 17:59:39 -0500

    fixed some migrations

--- db/migrate/20150218173301_create_game_table.rb
+++ db/migrate/20150218173301_create_game_table.rb
@@ -1,8 +1,6 @@
 class CreateGameTable < ActiveRecord::Migration
   def change
     create_table :games do |t|
-      t.integer :player1_id
-      t.integer :player2_id
       t.text :board
       t.integer :turn
       t.integer :winner
