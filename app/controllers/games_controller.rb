1d33ddc382ad68c1771a2730dafd2dac93618c76
samuel <samuel.whitley89@gmail.com>
2015-02-18 13:22:23 -0500

    updated games controller with create and join methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,34 @@
 class GamesControllers < ApplicationController
+  before_action :authenticate_user!, only: => [:join, :create]
   before_action :authenticate_user_from_token!
 
   def show
     @game = Game.find(params[:id])
     render json: { :game => @game }, status: :ok
+  end
+
+  def join
+    @waiting = Game.waiting.first
+    if @waiting 
+      @game = @waiting.users << current_user
+      render json:{:game => @game}, status: :ok
+    else
+      
+      @game = Game.create(player1_id: params[:id])
+    end
+  end
+
+
+
+  def create
+    @game = Game.create(game_params)
+    render json: {}, status: :created
+  end
+
+
+private
+  def game_params
+    require(:game).permit(:board, :player_id)
   end
 end
 
