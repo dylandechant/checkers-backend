7919c5390ba92f16ffdd69aa6bb8e5fc68f59c4c
Dylan Dechant <dylan.dechant@gmail.com>
2015-02-20 13:26:36 -0500

    add users to games when giving game to client

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,23 +1,26 @@
 
 class GamesController < ApplicationController
   
-  before_action :authenticate_user_from_token! , :only => [:join, :create]
+  before_action :authenticate_user_from_token!, :only => [:join, :create]
 
   def show
     @game = Game.find(params[:id])
-    render json: { :game => @game }, status: :ok
+    render json: { :game => @game , :users => @game.users }
+    # render json: :game => @game, :include => { :users }, status: :ok
+
   end
 
   def join
     @waiting = Game.waiting.first
     if @waiting 
       @waiting.users << current_user
-      render json: { :game => @waiting }, status: :ok
+      render json: { :game => @waiting, :users => @game.users }
+      # render json: :game => @waiting, :include => { :users => { :only => :email } }, status: :ok
     else
-      
       @game = Game.create
       @game.users = [current_user]
-      render json: { :game => @game }, status: :ok
+      render json: { :game => @game, :users => @game.users }
+      # render json: :game => @game, :include => { :users => { :only => :email } }, status: :ok
     end
   end
 
@@ -26,7 +29,8 @@ class GamesController < ApplicationController
   def create
     @game = Game.create
     @game.users = [current_user]
-    render json: { :game => @game }, status: :created
+    render json: { :game => @game,  :users => @game.users }
+    # render json: :game => @game, :include => { :users => { :only => :email } }, status: :created
   end
 
 
