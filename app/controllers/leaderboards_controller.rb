a1fd15dc7bff988a9d07ac12fc6753c5511e8e6d
Dylan Dechant <dylan.dechant@gmail.com>
2015-02-20 16:04:35 -0500

    add losses to losers

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,6 +1,6 @@
 class LeaderboardsController < ApplicationController
   def index
     @users = User.order(:wins => :desc).limit(10)
-    render json: {:users => @users.as_json(:only => [:email, :wins])}, status: :ok
+    render json: {:users => @users.as_json(:only => [:email, :wins, :losses])}, status: :ok
   end
 end
