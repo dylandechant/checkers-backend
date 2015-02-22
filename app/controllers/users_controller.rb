3adc924e200c01ac4d52c41f49d125e14bfd7cbf
Dylan Dechant <dylan.dechant@gmail.com>
2015-02-21 19:42:44 -0500

    renders json if user id is invalid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(:id => params[:id])
-    @user_games = @user.games
-    render json: {:user => @user.as_json(:only => [:email]), :game => @user_games }, status: :ok
+    if @user
+      @user_games = @user.games
+      render json: {:user => @user.as_json(:only => [:email]), :game => @user_games }, status: :ok
+    else
+      render json: { :error => "User does not exist"}, status: :not_found
+    end
   end
-
 end
